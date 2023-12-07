9bd048e094ff14c9e44edf89386591a8cba217e8
KGW2027 <retryer_@naver.com>
2023-12-07 20:27:07 +0900

    fix: 메인클래스를 StageBuilder를 사용하도록 수정

--- GLSpotlight/GLSpotlight.cpp
+++ GLSpotlight/GLSpotlight.cpp
@@ -1,21 +1,5 @@
-#include <thread>
-#include <gl/freeglut.h>
+#include "stage/StageBuilder.h"
 
-#include "renderer/GLSCircle.h"
-#include "music/MusicReader.h"
-#include "music/ParseLogger.h"
-
-GLSCircle circular = NULL;
-MusicReader music_reader;
-
-void init()
-{
-    circular = GLSCircle(1024);
-}
-
-void initMusic();
-void redisplay(int v);
-void display();
 
 /*
  * TODO 1 - dB 데이터가 제대로 적용되지 않은 것으로 보임.
@@ -29,48 +13,8 @@ void display();
  */
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitWindowSize(1280, 720);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutCreateWindow("GLSpotlight - Beta");
-
-    ParseLogger::getInstance();
-    init();
-    initMusic();
-    
-    glutDisplayFunc(display);
-    glutTimerFunc(50, redisplay, 0);
-    glutMainLoop();
+    StageBuilder builder = StageBuilder(argc, argv);
+    builder.start();
     
     return 0;
 }
-
-#pragma region Screen_Display
-
-void display()
-{
-    circular.render();
-}
-
-void redisplay(int v)
-{
-    glutPostRedisplay();
-    glutTimerFunc(50, redisplay, v);
-}
-
-#pragma endregion 
-
-#pragma region Music_Management
-
-void initMusic()
-{
-    const wchar_t* filename = L"../test5.wav";
-    music_reader = MusicReader(filename);
-    circular.set_music_reader(&music_reader);
-    std::thread render_thread(&GLSCircle::play, &circular);
-    std::thread music_thread(&MusicReader::play_music, &music_reader);
-    render_thread.detach();
-    music_thread.detach();
-}
-
-#pragma endregion
